d0be8d0aa2fc2b61f17bb66d5a259eefaef8480a
michelleamesquita <44561732+michelleamesquita@users.noreply.github.com>
2024-03-13 20:22:03 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python
 
 from setuptools import setup
+import pathlib
+
+import pkg_resources
+
+with pathlib.Path('requirements.txt').open() as requirements_txt:
+    install_requires = [
+        str(requirement)
+        for requirement
+        in pkg_resources.parse_requirements(requirements_txt)
+    ]
 
 setup(
     name='lib-test-sca',
@@ -9,5 +19,6 @@ setup(
     author='michelleamesquita',
     author_email='jfrog@jfrog.com',
     url='michelleamesquita/lib-test-sca',
-    install_requires=['PyYAML>3.11', 'nltk'],
+    install_requires=install_requires
+    #install_requires=['PyYAML>3.11', 'nltk'],
 )
