0f04ce03913c7d73fa67dbfc5ffa3349fe1b12f8
michelleamesquita <44561732+michelleamesquita@users.noreply.github.com>
2024-03-13 20:16:17 -0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='lib-test-sca',
+    version='1.0',
+    description='Project example for building Python project with JFrog products',
+    author='michelleamesquita',
+    author_email='jfrog@jfrog.com',
+    url='michelleamesquita/lib-test-sca',
+    install_requires=['PyYAML>3.11', 'nltk'],
+)
